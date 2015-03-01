5189ccc1b91a77e0f5da1381dc0bd516e5839105
Daryl Lau <daryl@goodeggs.com>
2015-02-28 21:35:42 -0800

    iterm2 scrolling

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -51,3 +51,17 @@
 (color-theme-sanityinc-tomorrow--define-theme night)
 
 (evil-mode 1)
+
+;; Enable mouse support
+(unless window-system
+  (require 'mouse)
+  (xterm-mouse-mode t)
+  (global-set-key [mouse-4] '(lambda ()
+                              (interactive)
+                              (scroll-down 1)))
+  (global-set-key [mouse-5] '(lambda ()
+                              (interactive)
+                              (scroll-up 1)))
+  (defun track-mouse (e))
+  (setq mouse-sel-mode t)
+)
